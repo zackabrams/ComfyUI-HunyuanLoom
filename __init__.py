c50596a59460c167fe6da492fbd798ba08c5aef8
logtd <logoftd@gmail.com>
2024-12-13 16:33:05 -0700

    init

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from .nodes.flow_edit_nodes import HyVideoFlowEditSamplerNode
+
+NODE_CLASS_MAPPINGS = {
+    "HyVideoFlowEditSampler": HyVideoFlowEditSamplerNode,
+}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "HyVideoFlowEditSampler": "HunyuanVideo Flow Edit Sampler",
+}
