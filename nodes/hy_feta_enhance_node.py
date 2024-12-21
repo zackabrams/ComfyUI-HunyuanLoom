f8a018ca0ef36e15e52cc01fc44dc423aa319e9c
logtd <logoftd@gmail.com>
2024-12-20 22:44:25 -0700

    all layers for enhance

--- nodes/hy_feta_enhance_node.py
+++ nodes/hy_feta_enhance_node.py
@@ -1,6 +1,6 @@
 DEFAULT_ATTN = {
     'double': [i for i in range(0, 100, 1)],#[0,1,2,3,4,5,6,7,9,11,13,15,17,19,21,23,25],
-    'single': [i for i in range(0, 100, 2)]
+    'single': [i for i in range(0, 100, 1)]
 }
 
 class HYFetaEnhanceNode:
